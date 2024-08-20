5e41a9d728ec2dc500a9914394501495be4af771
Sukh Singh <sukh@orbs.com>
2024-08-20 10:36:09 +0100

    fix dynamic route

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,17 @@
-export default async function BlogPostPage() {
-  return <div className="container mx-auto px-5"></div>
+export async function generateStaticParams() {
+  return [
+    {
+      slug: 'hello-world',
+    },
+  ]
+}
+
+export default async function BlogPostPage({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const { slug } = params
+
+  return <div className="container mx-auto px-5">{slug}</div>
 }
